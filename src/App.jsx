4c57269f88dcf0a711db1645c5805b06e357822e
Muhammad Hamza <zafarhamza652@gmail.com>
2025-01-10 00:56:50 +0500

    Till 34

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import './App.css'
+import "./App.css";
 
-import {
-  Router as Router,
-  Switch,
-  Route,
-} from "wouter";
+import { Router as Router, Switch, Route } from "wouter";
 
-import React, { Component } from 'react'
-import Navbar from './components/Navbar'
-import News from './components/News'
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 export default class App extends Component {
-  
   // state = {
   //   country: 'in', // Default country
   // };
@@ -20,27 +15,75 @@ export default class App extends Component {
   //   this.setState({ country: newCountry });
   // };
 
+  pageSize = 15;
   render() {
-    //const { country } = this.state; 
     return (
       <div>
         <Router>
-        <Navbar/>
-        {/* <Navbar onCountryChange={this.handleCountryChange}/> */}
-        {/* <News pageSize={9} country= "in" category="general" />  "in" */  }
-        <Switch>
-          <Route exact path="/"><News key="general" pageSize={9} country= "in" category="general" /></Route>
-          <Route exact path="/business"><News key="business" pageSize={9} country= "in" category="business" /></Route>
-           <Route exact path="/entertainment"><News key="entertainment" pageSize={9} country= "in" category="entertainment" /></Route>
-          <Route exact path="/health"><News key="health" pageSize={9} country= "in" category="health" /></Route>
-          <Route exact path="/science"><News key="science" pageSize={9} country= "in" category="science" /></Route>
-          <Route exact path="/sports"><News key="sports" pageSize={9} country= "in" category="sports" /></Route>
-          <Route exact path="/technology"><News key="technology" pageSize={9} country= "in" category="technology" /></Route>
-        </Switch>
-        {/* <Route exact path="/:country">{params => <News pageSize={9} country={params.country} />}</Route> */}
+          <Navbar />
+          {/* <Navbar onCountryChange={this.handleCountryChange}/> */}
+          {/* <News pageSize={this.pageSize} country= "in" category="general" />  "in" */}
+          <Switch>
+            <Route exact path="/">
+              <News
+                key="general"
+                pageSize={this.pageSize}
+                country="in"
+                category="general"
+              />
+            </Route>
+            <Route exact path="/business">
+              <News
+                key="business"
+                pageSize={this.pageSize}
+                country="in"
+                category="business"
+              />
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                key="entertainment"
+                pageSize={this.pageSize}
+                country="in"
+                category="entertainment"
+              />
+            </Route>
+            <Route exact path="/health">
+              <News
+                key="health"
+                pageSize={this.pageSize}
+                country="in"
+                category="health"
+              />
+            </Route>
+            <Route exact path="/science">
+              <News
+                key="science"
+                pageSize={this.pageSize}
+                country="in"
+                category="science"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <News
+                key="sports"
+                pageSize={this.pageSize}
+                country="in"
+                category="sports"
+              />
+            </Route>
+            <Route exact path="/technology">
+              <News
+                key="technology"
+                pageSize={this.pageSize}
+                country="in"
+                category="technology"
+              />
+            </Route>
+          </Switch>
+          {/* <Route exact path="/:country">{params => <News pageSize={this.pageSize} country={params.country} />}</Route> */}
         </Router>
       </div>
-
-    )
+    );
   }
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,107 +1,180 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
-
-  static defaultProps={
-    country: 'in',
+  static defaultProps = {
+    country: "in",
     pageSize: 8,
-    category: 'general',
-  }
+    category: "general",
+  };
 
-  static propTypes={
+  static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
+  };
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} - News Monkey`;
   }
 
-    constructor(){
-        super();
-        this.state = {
-            articles: [],
-            loading: false,
-            page: 1
-        }
-    }
-   async componentDidMount(){
-      let url= `https://newsapi.org/v2/top-headlines?sources?country=${this.props.country}&category=${this.props.category}&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=1&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      try {
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({ articles: parsedData.articles, loading: false ,totalArticles: parsedData.totalResults });
-      } 
-      catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    }
-    handlePreviousClick = async ()=> {
-      let url= `https://newsapi.org/v2/top-headlines?sources?country=${this.props.country}&&category=${this.props.category}&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-      try {
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({ articles: parsedData.articles });
-      } 
-      catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?sources?country=${this.props.country}&category=${this.props.category}&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
       this.setState({
-        // loading: true,
-        page:this.state.page-1
-
-      })
-    }
-    
-    handleNextClick = async ()=> {
-      
-      if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
-
-      }
-      else
-      {
-        let url= `https://newsapi.org/v2/top-headlines?sources?country=${this.props.country}&category=${this.props.category}&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;      
-      try {
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        // this.setState({loading: trure})
-        this.setState({ articles: parsedData.articles, loading: true });
-      } 
-      catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-
-      this.setState({
+        articles: parsedData.articles,
         loading: false,
-        page:this.state.page+1
-        
-      })
-      }
+        totalArticles: parsedData.totalResults,
+      });
+    } catch (error) {
+      console.error("Error fetching data: ", error);
     }
+  }
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  handlePreviousClick = async () => {
+    // let url = `https://newsapi.org/v2/top-headlines?sources?country=${
+    //   this.props.country
+    // }&&category=${
+    //   this.props.category
+    // }&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=${
+    //   this.state.page - 1
+    // }&pageSize=${this.props.pageSize}`;
+    // try {
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json();
+    //   this.setState({ articles: parsedData.articles });
+    // } catch (error) {
+    //   console.error("Error fetching data: ", error);
+    // }
+    // this.setState({
+    //   // loading: true,
+    //   page: this.state.page - 1,
+    // });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
+
+  handleNextClick = async () => {
+    // let url = `https://newsapi.org/v2/top-headlines?sources?country=${this.props.country}&category=${this.props.category}&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true });
+    // try {
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json();
+    //   this.setState({
+    //     articles: parsedData.articles,
+    //     loading: false,
+    //     totalArticles: parsedData.totalResults,
+    //   });
+    // } catch (error) {
+    //   console.error("Error fetching data: ", error);
+    // }
+    // if (
+    //   this.state.page + 1 >
+    //   Math.ceil(this.state.totalResults / this.props.pageSize)
+    // ) {
+    // } else {
+    //   let url = `https://newsapi.org/v2/top-headlines?sources?country=${
+    //     this.props.country
+    //   }&category=${
+    //     this.props.category
+    //   }&apiKey=8ed1458004ac478eb3f268d7a96c71e5&page=${
+    //     this.state.page + 1
+    //   }&pageSize=${this.props.pageSize}`;
+    //   try {
+    //     let data = await fetch(url);
+    //     let parsedData = await data.json();
+    //     // this.setState({loading: trure})
+    //     this.setState({ articles: parsedData.articles, loading: true });
+    //   } catch (error) {
+    //     console.error("Error fetching data: ", error);
+    //   }
+    //   this.setState({
+    //     loading: false,
+    //     page: this.state.page + 1,
+    //   });
+    // }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
   render() {
     return (
-        <>
+      <>
         <div className="container my-3">
-        <h2 className="text-center">NewsMonkey - Top headlines</h2>
-        {this.state.loading && <Spinner/>}
-        <div className="container my-3"></div>
-            <div className="row">
-            {!this.state.loading && this.state.articles.map((element) => {
-               return <div className="col-md-4 my-2" key={element.url}>
-              <NewsItem title= {element.title?element.title:""} description={element.description?element.description.slice(0,88):""} imgUrl={element.urlToImage? element.urlToImage: "https://techcrunch.com/wp-content/uploads/2024/07/GettyImages-1497617145-1.jpg?resize=1200,800"} newsUrl = {element.url}/>
-              </div> 
-            })}
-            </div>        
+          <h2 className="text-center">
+            NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+            Headlines
+          </h2>
+          {this.state.loading && <Spinner />}
+          <div className="container my-3"></div>
+          <div className="row">
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4 my-2" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : ""
+                      }
+                      imgUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://techcrunch.com/wp-content/uploads/2024/07/GettyImages-1497617145-1.jpg?resize=1200,800"
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+          </div>
         </div>
         <div className="container d-flex justify-content-between">
-        <button disabled={this.state.page <= 1} onClick={this.handlePreviousClick} className="btn btn-dark"> &larr; Previous</button>
-        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} onClick={this.handleNextClick} className="btn btn-dark">Next &rarr;</button>
+          <button
+            disabled={this.state.page <= 1}
+            onClick={this.handlePreviousClick}
+            className="btn btn-dark"
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            onClick={this.handleNextClick}
+            className="btn btn-dark"
+          >
+            Next &rarr;
+          </button>
         </div>
-        </>
+      </>
     );
   }
 }
-
 export default News;
